76120a37c91d4ebff4b7db7e1e32e2ad2625cf2a
Ana Clara <anaccustodio8@gmail.com>
2025-10-01 22:22:05 -0300

    feat: add background image and update HeroSection layout

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -1,8 +1,7 @@
 import React from 'react';
-import background from '../../assets/background.jpg';
 import logo from '../../assets/white-logo.png';
 import { Button } from '../ui/button';
-// import sound from '../../assets/sound.png';
+import sound from '../../assets/sound.png';
 
 interface HeroSectionProps {
 }
@@ -10,11 +9,14 @@ interface HeroSectionProps {
 const HeroSection: React.FC<HeroSectionProps> = ({
 }) => {
   return (
-    <section className='bg-cover bg-center' style={{ backgroundImage: `url(${background})` }}>
-        <header className='flex flex-col items-center'>
-            <img src={logo} alt='Logo DMVoz Produções' className='w-1/3' />
-            <h1 className='text-white text-center'>Transforme suas <b>ideias</b> em realidade com a <b>nossa voz</b></h1>
-            <Button>Saiba como funciona</Button>
+    <section className='absolute bg-[url(assets/background.png)] bg-cover bg-center'>
+        <header className='flex flex-col items-center gap-6 p-6'>
+            <img src={logo} alt='Logo DMVoz Produções' className='w-[110px]' />
+              <div className='flex flex-col items-center'>
+                <h1 className='text-white text-center text-md'>Transforme suas <b className="hover:text-primary">ideias</b> em realidade com a <b className="hover:text-primary">nossa voz!</b></h1>
+                <img src={sound} alt="Sound Wave" className='w-1/4' />
+              </div>
+            <Button size="sm" className="hover:bg-secondary cursor-pointer text-xs">Saiba como funciona</Button>
         </header>
     </section>
   );
