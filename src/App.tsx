e4c4dc530aed2f364c94a07656105d9eb3d1ad13
Ana Clara <anaccustodio8@gmail.com>
2026-01-21 16:39:39 -0300

    feat: add FAQSection component with accordion functionality and FAQ data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import AboutUsSection from "./components/AboutUsSection"
 import CampaignSection from "./components/CampaignSection"
+import FAQSection from "./components/FAQSection"
 import Footer from "./components/Footer"
 import HeroSection from "./components/HeroSection"
 import SpecialtiesSection from "./components/SpecialtiesSection"
@@ -15,6 +16,7 @@ function App() {
       <SpecialtiesSection />
       <AboutUsSection />
       <TestimonialsSection />
+      <FAQSection />
     </main>
     <Footer />
     </>
---
+++ src/components/FAQSection/index.tsx
@@ -0,0 +1,31 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+import faqData from "../../data/faq.json";
+import type { FAQs } from "../../types/faq";
+
+const FAQSection = () => {
+    return (
+        <section className="w-full flex flex-col gap-10 p-10 sm:px-15 sm:py-15 lg:flex-row lg:gap-20 lg:items-start lg:px-20 lg:py-20 xl:px-30 xl:gap-26 2xl:max-w-[1580px]">
+            <div>
+                <h2 className="text-2xl font-bold w-fit border-t-2 border-primary pt-2 mb-5 lg:text-4xl lg:mb-10 2xl:text-5xl">Tem alguma dúvida?</h2>
+                <p className="text-justify text-sm sm:max-w-md lg:text-lg">Confira as <b>perguntas frequentes</b> sobre nossos serviços! Se ainda restar alguma dúvida, é só chamar!</p>
+            </div>
+            <Accordion type="single" collapsible className="w-full rounded-lg border">
+                {(faqData as FAQs).map((item, index) => (
+                    <AccordionItem key={index} value={`item-${index}`} className="border-b px-4 last:border-b-0">
+                        <AccordionTrigger className="w-full cursor-pointer xl:text-base hover:no-underline hover:text-primary font-semibold data-[state=open]:text-primary">{item.question}</AccordionTrigger>
+                        <AccordionContent className="italic text-justify text-neutral-800 xl:text-base">
+                            {item.answer}
+                        </AccordionContent>
+                    </AccordionItem>
+                ))}
+            </Accordion>
+        </section>
+    )
+}
+
+export default FAQSection;
