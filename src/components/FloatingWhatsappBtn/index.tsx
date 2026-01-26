f7f113fdacb7ff96f11b16aca28d43a421522f24
Ana Clara <anaccustodio8@gmail.com>
2026-01-26 11:03:41 -0300

    feat: add profile picture for FloatingWhatsappBtn component

--- src/components/FloatingWhatsappBtn/index.tsx
+++ src/components/FloatingWhatsappBtn/index.tsx
@@ -1,16 +1,19 @@
 import { FloatingWhatsApp } from '@digicroz/react-floating-whatsapp'
+import profilePic from '../../assets/images/wpp-logo.png'
 
 const FloatingWhatsappBtn = () => {
     return (
         <FloatingWhatsApp
+            className='hidden min-[400px]:flex'
             phoneNumber='+5534998125957'
             accountName='DmVoz Produções'
-            avatar='/path/to/avatar.jpg'
+            avatar={profilePic}
             chatMessage='Olá, como posso te ajudar?'
             statusMessage='Online'
             placeholder='Digite a sua mensagem'
             chatboxHeight={370}
             notification={true}
+            notificationSound={true}
             allowClickAway={true}
             allowEsc={true}
         />
