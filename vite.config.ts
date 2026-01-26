3ab2b71d9a02cdd75dde6c546edefb621c158ea2
Ana Clara <anaccustodio8@gmail.com>
2026-01-26 13:07:19 -0300

    feat: add vite-plugin-imagemin for image optimization

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import viteImagemin from "vite-plugin-imagemin";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    viteImagemin({
+      gifsicle: { optimizationLevel: 3 },
+      optipng: { optimizationLevel: 5 },
+      pngquant: { quality: [0.6, 0.8] },
+      mozjpeg: { quality: 75 },
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
