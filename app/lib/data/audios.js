1aaca940bd8b1839d7d411a2dd909b9941259ccf
Ana Clara <anaccustodio8@gmail.com>
2026-03-19 16:08:43 -0300

    feat: update dependencies and add new audio and image assets

---
+++ app/lib/data/audios.js
@@ -0,0 +1,22 @@
+export const audios = [
+  {
+    title: "Demo Golden Shopping",
+    file: "/audios/audio_golden_shopping.mp3"
+  },
+  {
+    title: "Demo Petrobras",
+    file: "/audios/audio_petrobras.mp3"
+  },
+  {
+    title: "Demo Campanha Política",
+    file: "/audios/audio_politica.mp3"
+  },
+  {
+    title: "Demo Ministério da Saúde - Sarampo",
+    file: "/audios/audio_sarampo_ministerio_da_saude.mp3"
+  },
+  {
+    title: "Demo Shopping da Ilha",
+    file: "/audios/audio_shopping_da_ilha.mp3"
+  }
+];
