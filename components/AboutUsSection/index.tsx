3713ace4866dd67a6dd63850e6c28e8b7ff74d5b
Ana Clara <anaccustodio8@gmail.com>
2026-03-19 16:12:10 -0300

    feat: relocating sections components into new project

---
+++ components/AboutUsSection/index.tsx
@@ -0,0 +1,71 @@
+import mainImage from "@/public/images/pictures/main.png"
+import photo2 from "@/public/images/pictures/working.png"
+import photo3 from "@/public/images/pictures/soundTable.jpg"
+import photo4 from "@/public/images/pictures/microphone.png"
+import photo5 from "@/public/images/pictures/setup.jpg"
+import photo6 from "@/public/images/pictures/award.png"
+import Image from "next/image"
+
+const AboutUsSection = () => {
+    return (
+        <section className="w-full flex gap-20 p-10 sm:px-15 sm:py-15 md:flex-row md:items-center lg:px-20 lg:py-20 xl:px-30 xl:gap-26 2xl:max-w-[1580px]">
+            <div className="flex flex-col gap-12 md:w-1/2 md:gap-7 2xl:w-1/2">
+                <div>
+                    <h2 className="text-2xl font-bold w-fit border-t-2 border-primary pt-2 mb-5 lg:text-4xl 2xl:text-5xl">Sobre nós</h2>
+                    <p className="text-justify text-sm lg:text-base"><b>Denys</b> é a voz e a alma da DMVoz! Com vasta experiência no mercado publicitário, atua como <b>locutor, produtor e diretor,</b> oferecendo serviços de alta qualidade diretamente de seu Studio.</p>
+                </div>
+                <div className="border border-primary rounded-sm p-5">
+                    <h2 className="text-2xl font-semibold text-primary mb-3">Missão</h2>
+                    <p className="text-justify text-sm lg:text-base">Nossa missão é fornecer serviços de <b>locução, produção e direção</b> que agreguem valor ao mercado publicitário. Com dedicação e profissionalismo, buscamos criar conteúdos que envolvam e impactem o público, ajudando nossos clientes a alcançarem seus objetivos a partir da nossa comunicação.</p>
+                </div>
+                <div className="border border-secondary rounded-sm p-5 md:hidden xl:block">
+                    <h2 className="text-2xl font-semibold text-secondary mb-3">Visão</h2>
+                    <p className="text-justify text-sm lg:text-base">Ser reconhecido como uma referência no mercado publicitário, destacando-se pela <b>qualidade, inovação e comprometimento</b> em todos os projetos. Almejamos expandir nosso alcance, consolidando parcerias duradouras e inspirando confiança em todos os segmentos em que atuamos.</p>
+                </div>
+            </div>
+            <div className="hidden md:flex md:flex-col md:w-1/2">
+                <div className="grid grid-cols-3 auto-rows-auto gap-2 w-full h-auto">
+                    <Image
+                        src={mainImage}
+                        alt="Sobre nós principal"
+                        className="rounded-md object-cover col-span-2 row-span-2 h-auto"
+                    />
+
+                    <Image
+                        src={photo2}
+                        alt="Sobre nós 2"
+                        className="rounded-md object-cover h-full"
+                    />
+                    <Image
+                        src={photo3}
+                        alt="Sobre nós 2"
+                        className="rounded-md object-cover h-auto"
+
+                    />
+
+                    <Image
+                        src={photo4}
+                        alt="Sobre nós 3"
+                        className="rounded-md object-cover" 
+
+                    />
+
+                    <Image
+                        src={photo5}
+                        alt="Sobre nós 6"
+                        className="rounded-md object-cover"
+                    />
+
+                    <Image
+                        src={photo6}
+                        alt="Sobre nós 5"
+                        className="rounded-md object-cover"
+
+                    />
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default AboutUsSection;
---
+++ components/TestimonialsSection/components/TestimonialCard.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+
+interface TestimonialCardProps {
+    author: string;
+    testimonial: string;
+    logo: string;
+}
+
+const TestimonialCard = ({ author, testimonial, logo }: TestimonialCardProps) => {
+    return (
+        <div className="flex flex-col items-center justify-between gap-6 text-center h-full border border-[#1D0200] rounded-sm p-7 shadow-lg bg-orange-100 backdrop-blur-sm lg:min-h-[450px]">
+            <Image src={logo} alt={`Logo ${author}`} width={100} height={100} />
+            <p className="w-full text-sm lg:text-base">{testimonial}</p>
+            <span className="w-full text-sm font-bold mt-auto">- {author}</span>
+        </div>
+    )
+}
+
+export default TestimonialCard;
